39c05a46ab81401137210c4fe521e00398037de3
Jorg Visch <jorgvisch@gmail.com>
2023-05-11 13:27:07 +0200

    ignore build dir

--- the-jv-display/the-jv-display/image.ino
+++ the-jv-display/the-jv-display/image.ino
@@ -4,28 +4,65 @@
 */
 
 // Module image
+#include <FastLED.h>
+
+// settings
+
+// Number of leds in matrix
+#ifndef IMAGE_NUM_LEDS
+#define IMAGE_NUM_LEDS (16 * 16)
+#endif
+
+// Data pin of led matrix
+#ifndef IMAGE_DATA_PIN
+#define IMAGE_DATA_PIN 3
+#endif
+
+// RGB order of RGB leds (for WS2812B default GRB)
+#ifndef IMAGE_RGB_ORDER
+#define IMAGE_RGB_ORDER GRB
+#endif
+
+// Brightness (do not make it to bright, otherwise the led matrix can be damaged)
+const uint8_t imageBrightness{10};
+
+// the leds
+
+image_size_t imageCurrentIndex{0};
+CRGB imageLeds[IMAGE_NUM_LEDS];
 
 // --- Setup -----------
 
-void imageSetup() {
-  // TODO: custom initialization
-}        
-
-// --- Commands -----------
-
-void imageLoad() {
-  // TODO: implement command 'load'
+void imageSetup()
+{
+  FastLED.addLeds<WS2812B, IMAGE_DATA_PIN, IMAGE_RGB_ORDER>(imageLeds, IMAGE_NUM_LEDS);
 }
 
-void imageLoadDefault() {
+// --- Commands -----------
+void imageStartLoad()
+{
+  imageCurrentIndex = 0;
+}
+
+image_size_t imageLoad()
+{
+  image_size_t bytes_read{0};
+  while(Serial.available() > 3 && imageCurrentIndex < IMAGE_NUM_LEDS) {
+    auto pixel = &(imageLeds[imageCurrentIndex]);
+    bytes_read += Serial.readBytes(pixel, 3);
+    ++imageCurrentIndex;
+  }
+  return 10;
+}
+
+void imageLoadDefault()
+{
   // TODO: implement command 'loadDefault'
 }
 
-void imageShow() {
-  // TODO: implement command 'show'
+void imageShow()
+{
+  FastLED.show(imageBrightness);
 }
 
-void imageStartLoad() {
-  // TODO: implement command 'startLoad'
-}
 
