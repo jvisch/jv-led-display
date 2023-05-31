d052dd84866363ffe34a407d1f2c128cc5937a13
Jorg Visch <jorgvisch@gmail.com>
2023-05-31 15:49:28 +0200

    een beetje hekkerdehek, maar komt wel een keer goed

---
+++ the-jv-display/python-client/jv_display_pkg/src/jv_display/__init__.py
@@ -0,0 +1,4 @@
+from .display.display import Display, DisplayPart, Column, Row
+from .display.pixel import Pixel
+
+from .emulator.run import run as show_emulator
--- the-jv-display/python-client/jv_display_pkg/src/jv_display/emulator/run.py
+++ the-jv-display/python-client/jv_display_pkg/src/jv_display/emulator/run.py
@@ -1,16 +1,14 @@
 import tkinter
 from . import pixel_display_frame
 
-def show():
-    root = tkinter.Tk()
-    d = pixel_display_frame.PixelDisplayFrame(root)
-    d.pack(side="top", fill="both", expand=True)
-    root.mainloop()
-
 def run():
+    
+    def show():
+        root = tkinter.Tk()
+        d = pixel_display_frame.PixelDisplayFrame(root)
+        d.pack(side="top", fill="both", expand=True)
+        root.mainloop()
+    
     import threading
     t = threading.Thread(target=show)
     t.start()
-    
-if __name__ == "__main__":
-    show()
